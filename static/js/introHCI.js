f7cbe68f49b3df7edc1e37647243b4e3bfc214d9
Julian Yan <jwyan@ucsd.edu>
2021-01-21 10:51:45 -0800

    changed introHCI.css and added listener

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,4 +1,18 @@
 'use strict';
+function projectClick(e) {
+    // prevent the page from reloading     
+    e.preventDefault();
+    // In an event handler, $(this) refers to     
+    // the object that triggered the event     
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+		$(description).hide();
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+}
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -10,8 +24,10 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript is ready");
+		$(".jumbotron p").toggleClass("active");
 	});
+	$("a.thumbnail").click(projectClick);
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
